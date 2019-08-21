46b955d39a0196c3c16b52ee58d0a3a0d126175e
Yohanes Demissie <Yohanesdemissie@gmail.com>
2019-08-20 17:02:40 -0700

    Updates

---
+++ precache-manifest.43e873262fbd0aae42da2c3168815113.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53f6652ed65023f3691bdc0b2e8691f0",
+    "url": "/mr-texas-bbq/index.html"
+  },
+  {
+    "revision": "f4fedfc1dcfcffe269ec",
+    "url": "/mr-texas-bbq/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "8843e87a0c5654ee2abd",
+    "url": "/mr-texas-bbq/static/js/2.db518d06.chunk.js"
+  },
+  {
+    "revision": "f4fedfc1dcfcffe269ec",
+    "url": "/mr-texas-bbq/static/js/main.01cb61d8.chunk.js"
+  },
+  {
+    "revision": "9b5032ecfd2ca62fc38b",
+    "url": "/mr-texas-bbq/static/js/runtime~main.06dd4f2a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mr-texas-bbq/static/media/logo.5d5d9eef.svg"
+  }
+]);
