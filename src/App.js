c63f8a2356357ab9fed8cdd851f5ed76f64ea9a1
Yohanes Demissie <Yohanesdemissie@gmail.com>
2019-08-20 17:20:06 -0700

    added original src code to new repo for gh-pages deploy demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Home from './components/Home';
+import Products from './components/Products';
+import Locations from './components/Locations';
+import Contact from './components/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <React.Fragment>
+        <Route exact path="/" component={Home} />
+        <Route path="/products" component={Products} />
+        <Route path="/locations" component={Locations} />
+        <Route path="/contact" component={Contact} />
+      </React.Fragment>
+    </Router>
   );
 }
 
